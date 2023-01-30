892cc60a9574991673574af48313cbae2021a829
Natallia Kavalchuk <kov.natalyka@gmail.com>
2023-01-30 22:15:38 +0300

    changing account urls

---
+++ account/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from django.urls import path
+
+import account
+import events.views
+from account.views import ResetPasswordView, register_user
+
+urlpatterns = [
+    path('account/register', register_user, name='register'),
+    path('login/', account.views.login_user, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('password_reset/', ResetPasswordView.as_view(template_name='registration/password_reset.html'),
+         name='password_reset'),
+
+]
--- street_o/urls.py
+++ street_o/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls')),
+    path('account/', include('account.urls')),
 
 ]
